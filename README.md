4c99cf5dac4969ff4ccfe78658fccac79ed689f7
Prashant Tapase <tapase.prashant@gmail.com>
2019-08-04 21:49:01 +0530

    Node-red example

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # lead
+
+1. Import SQL file into MYSQL server.
+2. Dump mockdata file into D:// drive OR change location of mock data file path.
+3. Import flow.json into Node-red
+4. Deploy it.
+
+API endpoint
+1. /lead
+2. /mock?name=user3
---
+++ tbl_lead_user.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 04, 2019 at 03:59 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_loan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_lead_user`
+--
+
+DROP TABLE IF EXISTS `tbl_lead_user`;
+CREATE TABLE IF NOT EXISTS `tbl_lead_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `loan_amt` int(10) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
